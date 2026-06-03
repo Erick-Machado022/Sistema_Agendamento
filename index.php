b0f317c97d9ddd5b631f594299743a3a80d1979d
Erick-Machado022 <erickmachado050@gmail.com>
2026-06-02 22:42:13 -0300

    feat: converte index para PHP e inicia formulário de agendamento

--- index.php
+++ index.php
@@ -1,41 +1,9 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php
 
-    <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    include_once("config/url.php");
+    include_once("templates/header.php");
 
-    <!-- CSS -->
-    <link rel="stylesheet" href="style.css">
-
-    <title>Agendamentos</title>
-</head>
-
-<body>
-
-<!-- NAVBAR -->
-<header class="bg-white border-bottom">
-    <nav class="navbar px-3">
-        <div class="container-fluid">
-
-            <div class="d-flex align-items-center">
-                <img src="https://cdn-icons-png.flaticon.com/512/679/679720.png" class="logo-icon">
-                <strong>Agendamento Container</strong>
-            </div>
-
-            <div class="d-flex align-items-center gap-3 ms-auto">
-                <span>João Silva</span>
-
-                <button class="btn btn-outline-danger btn-sm">
-                    Sair
-                </button>
-            </div>
-
-        </div>
-    </nav>
-</header>
+?>
 
 <!-- HEADER (faixa cinza) -->
 <div class="header-titulo">
@@ -97,9 +65,9 @@
         <!-- BOTÃO -->
         <div class="col-md-3">
             <div class="card card-custom d-flex justify-content-center align-items-center h-100">
-                <button class="btn btn-primary w-100 btn-agendar">
-                    + Novo Agendamento
-                </button>
+                
+                   <a  class="btn btn-primary w-100 btn-agendar" href="<?= $BASE_URL ?>newAgendamento.php">+ Novo Agendamento</a>
+            
             </div>
         </div>
 
---
+++ newAgendamento.php
@@ -0,0 +1,13 @@
+<?php
+
+include_once("config/url.php");
+include_once("config/conexao.php");
+include_once("templates/header.php")
+
+?>
+
+<div class="header-titulo">
+    <div class="container">
+        <h2 class="fw-bold m-0">Novo Agendamento</h2>
+    </div>
+</div>
